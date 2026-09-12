5e3cb3f5612469008169f5b03451ff91b0c08940
Hugo Jesus <hugo.souza43@hotmail.com>
2026-09-11 22:43:27 -0300

    feat: adding put and delete routes

--- src/routes/food.ts
+++ src/routes/food.ts
@@ -1,14 +1,14 @@
 /* 
 Aqui devemos ter rotas para as seguintes funcionalidades:
 - Registro de nova refeição (post) [x]
-- Edição de refeição mapeada por ID (put)
-- Apagar refeição anterior (delete)
-- Listar refeições unitárias ou completas (get)
+- Edição de refeição mapeada por ID (put) [x]
+- Apagar refeição anterior (delete) [delete] [x]
+- Listar refeições unitárias ou completas (get) [x]
 - Recuperar metricas - lógica de soma por função (get) 
 */
 
 import { FastifyInstance } from "fastify";
-import z, { string } from "zod";
+import z from "zod";
 import { randomUUID } from "node:crypto";
 import { knexDb } from "../database";
 import { id } from "zod/v4/locales";
@@ -34,7 +34,38 @@ export async function foodRoutes(app: FastifyInstance) {
 
     return { listUniqueFood };
   });
+
   // PUT
+  app.put("/:id", async (request, reply) => {
+    const editFoodParamSchema = z.object({
+      id: z.uuid(),
+    });
+
+    const editFoodSchema = z.object({
+      name: z.string(),
+      description: z.string(),
+      date: z.iso.date(),
+      time: z.iso.time(),
+      inOutDiet: z.enum(["inDiet", "outDiet"]),
+    });
+
+    const { id } = editFoodParamSchema.parse(request.params);
+    const { name, description, date, time, inOutDiet } = editFoodSchema.parse(
+      request.body,
+    );
+
+    await knexDb("food_description")
+      .update({
+        name,
+        description,
+        date,
+        time,
+        inOutDiet,
+      })
+      .where({ id });
+
+    return reply.status(200).send("Refeição atualizada com sucesso!");
+  });
 
   // POST
   app.post("/create", async (request, reply) => {
@@ -63,4 +94,17 @@ export async function foodRoutes(app: FastifyInstance) {
   });
 
   // DELETE
+  app.delete("/:id", async (request) => {
+    const getUniqueFoodSchema = z.object({
+      id: z.uuid(),
+    });
+
+    const { id } = getUniqueFoodSchema.parse(request.params);
+
+    const deleteUniqueFood = await knexDb("food_description")
+      .delete()
+      .where({ id });
+
+    return { deleteUniqueFood };
+  });
 }
